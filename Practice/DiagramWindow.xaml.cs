386d3c7f6eee4fe634204d3b2b2476cb7b166094
KirillMoskvin <kirill.moskvin@mail.ru>
2017-07-08 00:05:39 +0300

    Нижняя часть интерфейса и окно для графиков

--- Practice/DiagramWindow.xaml.cs
+++ Practice/DiagramWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Practice
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для DiagramWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DiagramWindow : Window
     {
-        public MainWindow()
+        public DiagramWindow()
         {
             InitializeComponent();
-        }
-
-        private void btnDigram_Click(object sender, RoutedEventArgs e)
-        {
-            DiagramWindow dw = new DiagramWindow();
-            dw.Owner = this; //Чтобы окно закрывалось при закрытии главной формы
-            dw.Show();
         }
     }
 }
